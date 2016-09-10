948b93502b451b71220804328c862a97cbe0cd8e
muhdi <jemaltp@gmail.com>
2016-09-10 15:20:23 +1000

    jemal

--- client/spec/pages/teamPage.spec.js
+++ client/spec/pages/teamPage.spec.js
@@ -11,26 +11,20 @@ describe('Check-in dance', function() {
   });
 
   describe('initial git commit', function() {
-
     it('should include the text "Made by:"', function() {
       expect(page.$el).toContainText('Made by:');
     });
-
-
     it('should include the text "Nidhi Solanki"', function() {
       expect(page.$el).toContainText('Nidhi Solanki');
-    }); 
-
+    });
     it('should include the text "Jacqui Shadforth"', function() {
       expect(page.$el).toContainText('Jacqui Shadforth');
     });
-
     it('should include the text Jessie', function() {
       expect(page.$el).toContainText('Jessie');
     });
-
-    // TODO: Add a test to check for your name
-
-
+    it('should include the text Jemal', function() {
+      expect(page.$el).toContainText('Jemal');
+    });
   });
 });
