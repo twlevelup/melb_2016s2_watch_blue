c33f748494bbe97434a064777dbeb3d2a31bb962
muhdi <jemaltp@gmail.com>
2016-09-10 15:15:01 +1000

    Jemal

--- client/spec/pages/teamPage.spec.js
+++ client/spec/pages/teamPage.spec.js
@@ -14,15 +14,19 @@ describe('Check-in dance', function() {
     it('should include the text "Made by:"', function() {
       expect(page.$el).toContainText('Made by:');
     });
+
     it('should include the text "Nidhi Solanki"', function() {
       expect(page.$el).toContainText('Nidhi Solanki');
     });
+
     it('should include the text "Jacqui Shadforth"', function() {
       expect(page.$el).toContainText('Jacqui Shadforth');
     });
+
     it('should include the text Jessie', function() {
       expect(page.$el).toContainText('Jessie');
     });
+
     it('should include the text Jemal', function() {
       expect(page.$el).toContainText('Jemal');
     });
