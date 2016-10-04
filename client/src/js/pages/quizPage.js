dcbbbe08d590d26e362027d47333348b2722b2ce
jacquelineshadforth <s3332777@student.rmit.edu.au>
2016-10-04 20:51:59 +1100

    [Jemal :pear: Jacqui] Add quiz shapes functionality

--- client/src/js/pages/quizPage.js
+++ client/src/js/pages/quizPage.js
@@ -1,13 +1,23 @@
 'use strict';
 
 var Page = require('watch_framework').Page;
+
+var shapes = ['<img src=\'client/src/images/square.png\' alt=\'square\' class=\'small\'>',
+                '<img src=\'client/src/images/circle.png\' alt=\'circle\' class=\'small\'>',
+                '<img src=\'client/src/images/diamond.png\' alt=\'diamond\' class=\'small\'>',
+                '<img src=\'client/src/images/triangle.png\' alt=\'triangle\' class=\'small\'>'];
+
 var quizPage = Page.extend({
 
     id: 'quizPage',
     template: require('../../templates/pages/quiz.hbs'),
-    objects: { title: 'Quiz Question Page' },
+    objects: { title: 'Quiz Page' },
     render: function() {
         this.$el.html(this.template(this.objects));
+        this.$el.find('#display-shape').html(shapes);
+        this.$el.find('#shape-scroll').animate({
+            margin: '0 1000px 0 0'
+          });
         return this;
       }
 
