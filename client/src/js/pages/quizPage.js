99fdce52093c209c069a6d2a73fb4ab8f1a387ce
jacquelineshadforth <s3332777@student.rmit.edu.au>
2016-10-22 11:28:42 +1100

    [Jacqui and Adrian] Add randomisation to question page

--- client/src/js/pages/quizPage.js
+++ client/src/js/pages/quizPage.js
@@ -2,24 +2,43 @@
 
 var Page = require('watch_framework').Page;
 
-var shapes = ['<div class=\'square large\'>',
-                '<div class=\'circle large\'>',
-                '<div class=\'diamond large\'>',
-                '<div class=\'triangle large\'>'];
+var shapes = ['square', 'circle', 'diamond', 'triangle'];
 
 var quizPage = Page.extend({
 
     id: 'quizPage',
     template: require('../../templates/pages/quiz.hbs'),
-    objects: { title: '' },
+    objects: { title: '', questionShapes: '' },
+    difficulty: 4,
+    question: [],
     render: function() {
         this.$el.html(this.template(this.objects));
-        this.$el.find('#display-shape').html(shapes);
+        this.$el.find('#display-shape').html(this.objects.questionShapes);
         this.$el.find('#shape-scroll').animate({
             margin: '0 1000px 0 0'
           });
         return this;
+      },
+    initialize: function () {
+      for (var i = 1; i <= this.difficulty; i++) {
+        var randomNumber = Math.floor(Math.random() * (shapes.length)) % 4;
+        this.question.push(shapes[randomNumber])
+        this.objects.questionShapes += this.generateShapeHTML(shapes[randomNumber]);
       }
+      console.log(this.objects.questionShapes);
+      this.render();
+      var self = this;
+      setTimeout(function() {
+        self.goToAnswerPage(); 
+      }, 7000);
+    },
+    generateShapeHTML: function(shape) {
+      return '<div class=\'' + shape +' large\'></div>'
+    },
+    goToAnswerPage: function() {
+      window.App.data.question = this.question;
+      window.App.navigate('answer');
+    }
 
   });
 
